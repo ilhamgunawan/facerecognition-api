364582b0d9da37595c182e38e4369f33e681fd64
Ilham Muhammad <ilhammrg@gmail.com>
2020-06-05 07:08:57 +0700

    Add register controllers

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const knex = require('knex');
 const bcrypt = require('bcrypt-nodejs');
+const register = require('./controllers/register');
 
 const app = express();
 
@@ -39,31 +40,7 @@ app.post('/signin', (req, res) => {
         .catch(err => res.status(400).json('Wrong credentials.'));
 });
 
-app.post('/register', (req, res) => {
-    const { email, password, name } = req.body;
-    const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-        trx.insert({
-            hash: hash,
-            email: email
-        })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-            return trx('users')  
-                .returning('*')
-                .insert({
-                    email: loginEmail[0],
-                    name: name,
-                    joined: new Date()
-            })
-            .then(user => res.json(user[0]));
-        })
-        .then(trx.commit)
-        .catch(trx.rollback);
-    })
-    .catch(err => res.status(400).json('Unable to Sign Up'));
-});
+app.post('/register', (req, res) => register.handleRegister(req, res, db, bcrypt));
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
