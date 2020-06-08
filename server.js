9bdd56f287dbae97c60609fbbf315ba5411fa2fb
Ilham Muhammad <ilhammrg@gmail.com>
2020-06-08 10:38:33 +0700

    update database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const app = express();
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'admin',
-      password : '159357',
-      database : 'face-recognition-db'
+      connectionString : process.env.DATABASE_URL,
+      ssl: true
     }
 });
 
